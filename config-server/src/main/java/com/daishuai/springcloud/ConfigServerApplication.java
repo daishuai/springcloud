763ed0de8aaa2556ecf7eeb2bb4f4ac7ce9265d6
daishuai <867390427@qq.com>
2018-05-26 18:49:51 +0800

    Feign

---
+++ config-server/src/main/java/com/daishuai/springcloud/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.daishuai.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Description: java类作用描述
+ * @Author: daishuai
+ * @CreateDate: 2018/5/26 18:32
+ * @Version: 1.0
+ * Copyright: Copyright (c) 2018
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+}
