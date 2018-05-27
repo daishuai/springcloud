31b0ba8a607a0e2a8c575cdd2b0bd87d0965aa23
daishuai <867390427@qq.com>
2018-05-27 20:02:06 +0800

    Feign

--- config-client/src/main/java/com/daishuai/springcloud/ConfigClientApplication.java
+++ config-client/src/main/java/com/daishuai/springcloud/ConfigClientApplication.java
@@ -2,20 +2,19 @@ package com.daishuai.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Description: java类作用描述
  * @Author: daishuai
- * @CreateDate: 2018/5/26 18:32
+ * @CreateDate: 2018/5/26 19:14
  * @Version: 1.0
  * Copyright: Copyright (c) 2018
  */
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
-
+@EnableEurekaClient
+public class ConfigClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigClientApplication.class, args);
     }
 }
